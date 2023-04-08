1da076ffd056e9e6be7937269496cb5e35a1febf
pujaKundu <iampujakundu@gmail.com>
2023-04-08 23:50:08 +0600

    search bar

--- components/Shared/Navbar.js
+++ components/Shared/Navbar.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useState } from "react";
 
 const Navbar = () => {
@@ -26,7 +27,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="py-5 flex justify-between bg-transparent top-0 sticky z-20">
+    <div className="py-5 bg-zinc-900 flex justify-between  top-0 sticky z-20">
       <div className="flex items-center ml-5">
         <img className="w-12 h-12" src="/assets/movie-reel.png" alt="" />
         <h1 className="text-2xl text-cyan-300">Silver Screen</h1>
@@ -39,24 +40,26 @@ const Navbar = () => {
           onChange={handleSearch}
         />
         {showModal && (
-          <div className="fixed mt-2  z-50 bg-black bg-opacity-50 flex justify-center items-center">
+          <div
+            className="fixed mt-2 w-96 z-100 bg-black bg-opacity-50 flex justify-center items-center overflow-hidden mr-5"
+            style={{ maxHeight: "100vh", overflowY: "auto" }}
+          >
             <div className="bg-zinc-800 p-8 rounded shadow-lg">
               <h2 className="text-lg text-slate-300 font-medium mb-4">
                 Search results for {searchInput}
               </h2>
               <ul className="space-y-4">
                 {searchResults.map((movie) => (
-                  <div
-                    className="flex items-center justify-start"
-                    key={movie.id}
-                  >
-                    <img
-                      className="w-10 h-10 rounded mr-2"
-                      src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
-                      alt=""
-                    />
-                    <li className="text-slate-300">{movie.title}</li>
-                  </div>
+                  <Link key={movie.id} href={`/movies/${movie.id}`}>
+                    <div className="flex items-center justify-start cursor-pointer hover:bg-zinc-700 rounded p-3">
+                      <img
+                        className="w-10 h-10 rounded mr-2"
+                        src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
+                        alt=""
+                      />
+                      <li className="text-slate-300">{movie.title}</li>
+                    </div>
+                  </Link>
                 ))}
               </ul>
               <button
